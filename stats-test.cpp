7a53abac8b892894d50411dfa03496f32c76f049
320088280 <320088280@INGBTCPIC5NB405>
2020-09-07 13:15:46 +0530

    modified files to represent the test-case behavior

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,22 +2,27 @@
 
 #include "catch.hpp"
 #include "stats.h"
-
 #include <cmath>
 
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
+	std::vector<float> number_set{ 1.5f, 8.9f, 3.2f, 4.5f };
+    auto computedStats = Statistics::ComputeStatistics(number_set);
+    float epsilon = 0.001f;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
-    auto computedStats = Statistics::ComputeStatistics({});
+	std::vector<float> number_set{};
+    auto computedStats = Statistics::ComputeStatistics(number_set);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
+
+	REQUIRE(std::isnan(computedStats.min) == true);
+	REQUIRE(std::isnan(computedStats.max) == true);
+	REQUIRE(std::isnan(computedStats.average) == true);
 }
--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,27 @@
 #include "stats.h"
+#include <algorithm>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(std::vector<float>& number_set)
+{
+	Stats stats{};
+	if(!number_set.empty())
+	{
+		float sum = 0;
+
+		for(auto number: number_set)
+		{
+			sum += number;		
+		}
+		stats.average = sum / number_set.size();
+
+		std::sort(number_set.begin(), number_set.end());
+
+		stats.min = number_set.front();
+		stats.max = number_set.back();
+
+		return stats;		
+	}
+	stats.average = stats.max = stats.min = NAN;
+	return stats;
 }
