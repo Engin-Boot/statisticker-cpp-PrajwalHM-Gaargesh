19e772d59f54745420673f848c6070bca1493104
320088280 <320088280@INGBTCPIC5NB405>
2020-09-07 19:58:35 +0530

    Modified ComputeStats file to consider NaN values

--- stats.cpp
+++ stats.cpp
@@ -5,14 +5,24 @@
 Statistics::Stats Statistics::ComputeStatistics(std::vector<float>& number_set)
 {
 	Stats stats{};
-	if(!number_set.empty())
+	
+	float sum = 0.0f;
+	auto number = number_set.begin();
+	
+	while(number != number_set.end())
 	{
-		float sum = 0;
-
-		for(auto number: number_set)
+		if (!std::isnan(*number))
 		{
-			sum += number;		
+			sum += *(number);
+			++number;
 		}
+		else
+		{
+			number = number_set.erase(number);
+		}
+	}
+	if(sum != static_cast<float>(0))
+	{
 		stats.average = sum / number_set.size();
 
 		std::sort(number_set.begin(), number_set.end());
@@ -20,8 +30,8 @@ Statistics::Stats Statistics::ComputeStatistics(std::vector<float>& number_set)
 		stats.min = number_set.front();
 		stats.max = number_set.back();
 
-		return stats;		
-	}
+		return stats;
+	}		
 	stats.average = stats.max = stats.min = NAN;
 	return stats;
 }
